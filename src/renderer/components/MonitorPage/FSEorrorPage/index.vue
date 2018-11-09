15ec2feae9ddabfd7b54fcbbaf77e5ac9a171ab8
xiexin12138 <xiexin344@163.com>
2018-11-09 11:59:33 +0800

    修改背景颜色

--- src/renderer/components/MonitorPage/FSEorrorPage/index.vue
+++ src/renderer/components/MonitorPage/FSEorrorPage/index.vue
@@ -27,7 +27,7 @@
 				<el-col :span="8":offset="4"><div style="text-align:right;">文件系统状态:</div></el-col>
 				<el-col :span="8" ><div>{{fs.status}}</div></el-col>
 			</el-row> -->
-			
+
 			</el-dialog>
 			<el-dialog
 			  title="error信息"
@@ -43,7 +43,7 @@
 				<el-col :span="8":offset="2"><div style="text-align:right;">产生时间:</div></el-col>
 				<el-col :span="14" ><div>{{errorObj.time}}</div></el-col>
 			</el-row>
-			
+
 			</el-dialog>
 		</el-col>
 		<log-list :ListData="ListData" class="log_list"></log-list>
@@ -74,7 +74,7 @@
 </style>
 <script>
 /**
- * 
+ *
 1、事先生成固定数量的随机大小的圆
 2、获取数据，将数据和圆绑定，对需要显示的数据进行标记
 3、显示未处理的错误和被处理的错误
@@ -91,7 +91,7 @@ import Bar from '@/components/common/Bar'
 import Vue from 'vue'
 import { Message, Row, Col, Dialog } from 'element-ui'
 import d3 from 'd3'
-import * as status from '../../../api/status' 
+import * as status from '../../../api/status'
 import LogList from './LogList'
 import Foot from '@/components/common/Foot'
 Vue.use(Row)
@@ -114,11 +114,11 @@ function shuffle(array){
 }
 
 // 原设计
-// var circleColorList = ["#F0F8FF","#F5FFFA","#FFFFF0"]; 
-// var borderColorList = ["#87CEFA","#B4EEB4","#FFEC8B"]; 
+// var circleColorList = ["#F0F8FF","#F5FFFA","#FFFFF0"];
+// var borderColorList = ["#87CEFA","#B4EEB4","#FFEC8B"];
 // 演示
-var circleColorList = ["#F0F8FF","#F0FFFA","#FFFFF0","#FFFFF0"]; 
-var borderColorList = ["#87CEFA","#00EEB4","#ed7d31","#FFD700"]; 
+var circleColorList = ["#F0F8FF","#F0FFFA","#FFFFF0","#FFFFF0"];
+var borderColorList = ["#87CEFA","#00EEB4","#ed7d31","#FFD700"];
 var abnormalCircleColor="#FFFAFA";
 // var abnormalBorderColor="#F08080";
 var abnormalBorderColor="#f20c00";
@@ -126,32 +126,32 @@ var abnormalBorderColor="#f20c00";
 var circleColorIndex=0;
 var borderColorIndex=0;
 
-function getCircleColor(colorList){ 	
-	var color = colorList[circleColorIndex]; 
-	
-	circleColorIndex=circleColorIndex+1; 
+function getCircleColor(colorList){
+	var color = colorList[circleColorIndex];
+
+	circleColorIndex=circleColorIndex+1;
 	if(circleColorIndex==4){
 		circleColorIndex=0;
 	}
-    return color; 
-} 
+    return color;
+}
 
 
-function getBordereColor(colorList){ 	
-	var color = colorList[borderColorIndex]; 
-	
-	borderColorIndex=borderColorIndex+1; 
+function getBordereColor(colorList){
+	var color = colorList[borderColorIndex];
+
+	borderColorIndex=borderColorIndex+1;
 	if(borderColorIndex==4){
 		borderColorIndex=0;
 	}
-    return color; 
-} 
+    return color;
+}
 
 
 // 圆的色彩显示函数，暂定根据层级显示颜色
 function circleColor(d){
 	if (d.depth == 0) {
-		return '#fff'   //最底层
+		return '#f9fafc'   //最底层
 	}else if (d.depth == 1) {   //文件系统
 		var color= getCircleColor(circleColorList);
 		return color
@@ -200,13 +200,13 @@ function circleOpacity(d){
 	}
 }
 // 目前采用的display显示方法
-function circleDisplay(d){	
+function circleDisplay(d){
 	if (d.state > 0) {
 		return 'display:block;'
 	} else if (d.state == 0 ) {
 		return 'display:none;'
 	}
-	
+
 }
 function circleRadius(d){
 	if (d.depth == 1) {
@@ -217,29 +217,29 @@ function circleRadius(d){
 }
 
 function handleList(init_data){
-	function sortNumber(a,b) { 
-		return -(a[1] - b[1]) 
-	} 
+	function sortNumber(a,b) {
+		return -(a[1] - b[1])
+	}
 	function handle_errorListData(init_data){
 		let fs_error_set=[];   //各文件系统下记录各error详细信息条目的集合
 		let errortime_set=[];  //errortime_set中每个元素包含两个元素:[对应fs_error_set中条目的序号, error发生的时间]
 		let latest_time=[];
-		let error_index=[];    
+		let error_index=[];
 	    let latest_errorData=[];
 
 		for (let fs_set of init_data){
-		   
+
 		    for(let fs_error_detail of fs_set.fs_error){
 		       fs_error_set.push(fs_error_detail);
 		    }
-		       
+
 		}
 
 		for (let [index, elem] of fs_error_set.entries()) {
 		  let seconds_total=Date.parse(elem.time); //转换时间格式,解析一个表示日期的字符串，并返回从 1970-1-1 00:00:00 所经过的毫秒数。
 
 		  let errortime= [index, seconds_total];
-		  errortime_set.push(errortime);  
+		  errortime_set.push(errortime);
 		}
 
 
@@ -289,11 +289,11 @@ export default {
 		}
 	},
 	mounted: function() {
-		
+
 		this.init()
 		this.updateGraph()
 		this.bindData()  // 这个操作已经包含updateGraph，不过上面是为了初次渲染所以必须有一个
-		
+
 		// setTimeout(()=>{
 		// 	console.log('begin')
 		// 	this.bindData()
@@ -357,21 +357,21 @@ export default {
 			d3.select('.descrition')
 				// .attr('transform', 'translate(-45,0)')
 				.append('g')
-				.selectAll("rect")  
-	          	.data(dataset)  
-	          	.enter()  
-	          	.append("rect")  
-	        	.attr("y",50)//每个矩形的起始x坐标  
-	        	.attr("x",function(d,i){//每个矩形的起始y坐标  
-	                return i * recWidth;  
-	          	})  
-	        	.attr("width",function(d){//每个矩形的宽度   
-	                return 10;  
-	         	})  
-	        	.attr("height",recWidth-2)//每个矩形的高度  
-	        	.attr("fill",function(d,i){//每个矩形的颜色 
+				.selectAll("rect")
+	          	.data(dataset)
+	          	.enter()
+	          	.append("rect")
+	        	.attr("y",50)//每个矩形的起始x坐标
+	        	.attr("x",function(d,i){//每个矩形的起始y坐标
+	                return i * recWidth;
+	          	})
+	        	.attr("width",function(d){//每个矩形的宽度
+	                return 10;
+	         	})
+	        	.attr("height",recWidth-2)//每个矩形的高度
+	        	.attr("fill",function(d,i){//每个矩形的颜色
                     return borderColorList[i]
-	          	});//填充颜色 
+	          	});//填充颜色
 
 			d3.select('.descrition')
 				.append('g')
@@ -386,21 +386,21 @@ export default {
 				.attr('font-size', '20px')
 			d3.select('.descrition')
 				.append('g')
-				.selectAll("rect")  
-	          	.data([1])  
-	          	.enter()  
-	          	.append("rect")  
-	        	.attr("y",80)//每个矩形的起始x坐标  
-	        	.attr("x",function(d,i){//每个矩形的起始y坐标  
-	                return i * recWidth;  
-	          	})  
-	        	.attr("width",function(d){//每个矩形的宽度   
-	                return 10;  
-	         	})  
-	        	.attr("height",recWidth-2)//每个矩形的高度  
-	        	.attr("fill",function(d,i){//每个矩形的颜色 
+				.selectAll("rect")
+	          	.data([1])
+	          	.enter()
+	          	.append("rect")
+	        	.attr("y",80)//每个矩形的起始x坐标
+	        	.attr("x",function(d,i){//每个矩形的起始y坐标
+	                return i * recWidth;
+	          	})
+	        	.attr("width",function(d){//每个矩形的宽度
+	                return 10;
+	         	})
+	        	.attr("height",recWidth-2)//每个矩形的高度
+	        	.attr("fill",function(d,i){//每个矩形的颜色
 	                return abnormalBorderColor
-	          	});//填充颜色  
+	          	});//填充颜色
 			d3.select('.descrition')
 				.append('g')
 				.selectAll('text')
@@ -415,16 +415,16 @@ export default {
 		},
 
 		updateData() {
-			// 
+			//
 		},
 		async bindData() {
 			// let data = this.updateData()
 			let data = await status.MonitorFS()
 
 			this.ListData = handleList(data)
-			
+
 			for (let i = 0; i < data.length; i++) {
-				this._root.children[i].name = data[i].fs_name 
+				this._root.children[i].name = data[i].fs_name
 				for (let j = 0; j < data[i].fs_error.length; j++) {
 					this._root.children[i].children[j].name =
 						data[i].fs_error[j].file_name
@@ -440,7 +440,7 @@ export default {
 			}
 			await this.updateGraph()
 		},
-		
+
 		//初始四个大圆代表四个不同的文件系统
 		initGraphData() {
 			let cc1 = {
@@ -548,7 +548,7 @@ export default {
 					}
 				})
 				.attr('style',circleDisplay)
-				
+
 			 this.animationRecover()        //执行修复动画
 		},
 
