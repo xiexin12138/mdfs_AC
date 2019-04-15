9d1dd1e9d38c7957a2bfe6c41dd15720e0434f02
xiexin12138 <xiexin344@163.com>
2019-04-15 10:29:36 +0800

    修改cm的ip

--- src/renderer/api/socket.js
+++ src/renderer/api/socket.js
@@ -16,7 +16,7 @@ console.log("【本地测试】" + type.LOCAL_TEST);
 // } else {
 configObj = {
   hostBackup: '219.223.197.76', //'219.223.197.76'(haiyang),219.223.195.100(yuan),219.223.193.22(test),219.223.197.89
-  host: '219.223.197.76'
+  host: '219.223.197.127'
   /*host:'0.0.0.0',
  			hostBackup:'0.0.0.0'*/
 }
@@ -45,8 +45,8 @@ class Socket {
     this.host = configObj.host || '219.223.199.154'
     // this.host = '219.223.192.110'
     this.hostBackup = configObj.hostBackup || '192.168.1.11'
-    // this.port = 10086
-    this.port = 10287
+    this.port = 10086
+    // this.port = 10287
     /**
      * 新建一个socket
      * @type {net}
@@ -118,7 +118,6 @@ class Socket {
     arr.push(num)
     let buf2 = Buffer.from(arr)
     let buf3 = Buffer.concat([buf2, buf])
-    console.log("buf3",buf3);
     this._socket.write(buf3)
   }
   /**
